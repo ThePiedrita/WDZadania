85dc2c471ae26ed6b0c72750b355e74832b24c67
ThePiedrita <118008754+ThePiedrita@users.noreply.github.com>
2023-04-26 18:39:36 +0200

    panda

---
+++ pandas_lab.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+s = pd.Series([1, 3, 5, np.nan, 6, 8])
+print(s)
+s = pd.Series([10, 12, 8, 14], index=['a', 'b', 'c', 'd'])
+print(s)
+data = {'Kraj': ['Belgia', 'Indie', 'Brazylia'],'Stolica': ['Bruksela', 'New Delhi', 'Brasilia'],'Populacja': [11190846, 1303171035, 207847528]}
+df = pd.DataFrame(data)
+print(df)
+print(df.dtypes)
+daty = pd.date_range('20210324', periods=5)
+print(daty)
+df = pd.DataFrame(np.random.randn(5,4), index=daty, columns=list('ABCD'))
+print(df)
+xlsx=pd.ExcelFile('imiona.xlsx')
+df=pd.read_excel(xlsx,header=0)
+print(df)
+zam=pd.read_csv('zamowienia.csv',header=0, sep=';',decimal=',')
+print(zam)
